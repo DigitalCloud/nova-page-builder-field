d3f095b5d1a8c4a7ca765ea0cd1f9f180a4d5b25
Behzod <43970199+behzodjon@users.noreply.github.com>
2021-08-27 10:58:09 +0500

    add componnent

---
+++ resources/js/components/IndexField.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="field.asHtml" v-html="value"></div>
+    <span v-else class="whitespace-no-wrap">{{ value }}</span>
+</template>
+
+<script>
+export default {
+    props: ['resourceName', 'field'],
+    computed: {
+        value() {
+            return this.field.value[this.field.indexLocale] || '—'
+        }
+    }
+}
+</script>
